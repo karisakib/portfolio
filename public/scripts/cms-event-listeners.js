d93d0828abe703a695183dfd8e6800ae08e98dba
Kari Sakib <karisakib@gmail.com>
2024-07-17 20:13:05 -0400

    CMS.init() causing errors!!!!

--- public/scripts/cms-event-listeners.js
+++ public/scripts/cms-event-listeners.js
@@ -1,28 +1,32 @@
-import CMS from 'decap-cms-app';
+// import CMS from 'decap-cms-app';
 
-// Register an event listener for entry creation
-CMS.registerEventListener({
-  name: 'preSave',
-  handler: ({ entry }) => {
-   console.log(entry.get('slug'));
-   const titleSlug = entry.getIn(['data', 'title'], '').toLowerCase().replace(/[']/g, '').replace(/[.]/g, '-').replace(/[\s]/g, '-')
-   const slug = entry.get('data').set('slug', titleSlug);
-   console.log(slug);
-  }
-});
-
+// // Register an event listener for entry creation
 // CMS.registerEventListener({
-//  name: 'preSave',
-//  handler: ({ entry }) => {
-//   if (entry.get('slug')) {
-//     return entry.get('data').set('titleslug', entry.get('slug'));
-//   } else {
-//     // you can use any relevant field/logic for generating the initial slug
-//     const titleSlug = entry.getIn(['data', 'title'], '').toLowerCase().replace(/[']/g, '').replace(/[.]/g, '-').replace(/[\s]/g, '-')
-//     return entry.get('data').set('titleslug', titleSlug);
+//   name: 'preSave',
+//   handler: ({ entry }) => {
+//    console.log(entry.get('slug'));
+//    const titleSlug = entry.getIn(['data', 'title'], '').toLowerCase().replace(/[']/g, '').replace(/[.]/g, '-').replace(/[\s]/g, '-')
+//    const slug = entry.get('data').set('slug', titleSlug);
+//    console.log(slug);
 //   }
-//  },
 // });
 
-// Initialize CMS
-CMS.init();
+// // CMS.registerEventListener({
+// //  name: 'preSave',
+// //  handler: ({ entry }) => {
+// //   if (entry.get('slug')) {
+// //     return entry.get('data').set('titleslug', entry.get('slug'));
+// //   } else {
+// //     // you can use any relevant field/logic for generating the initial slug
+// //     const titleSlug = entry.getIn(['data', 'title'], '').toLowerCase().replace(/[']/g, '').replace(/[.]/g, '-').replace(/[\s]/g, '-')
+// //     return entry.get('data').set('titleslug', titleSlug);
+// //   }
+// //  },
+// // });
+
+
+// /**
+//  * I HAVE NO IDEA WHY THIS IS CAUSING ISSUES
+//  */
+// // Initialize CMS
+// CMS.init({config:"/admin/config.yml"});
