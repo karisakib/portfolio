7cb141801bf633bf4de45728b7fdfda5eace0ed0
Kari Sakib <karisakib@gmail.com>
2024-07-17 23:25:40 -0400

    nope, preSave slug is not gonna work

--- public/scripts/cms-event-listeners.js
+++ public/scripts/cms-event-listeners.js
@@ -14,18 +14,21 @@ const generateSlug = (title) => {
 
 // Register the preSave event listener
 CMS.registerEventListener({
-  name: 'preSave',
-  handler: ({ entry }) => {
-    // Get the title from the entry
-    const title = entry.getIn(['data', 'title']);
-    
-    // Generate the slug
-    const slug = generateSlug(title);
+ name: 'preSave',
+ handler: ({ entry }) => {
+   // Get the title from the entry
+   const title = entry.getIn(['data', 'title']);
+   console.log('Title:', title);
+   
+   // Generate the slug
+   const slug = generateSlug(title);
+   console.log('Generated Slug:', slug);
 
-    // Set the slug in the entry data
-    return entry.get('data').set('slug', slug);
-  }
+   // Set the slug in the entry data and return the modified entry
+   return entry.setIn(['data', 'slug'], slug);
+ }
 });
+
 
 // Initialize CMS
 // CMS.init({ config: "../admin/config.yml" });
